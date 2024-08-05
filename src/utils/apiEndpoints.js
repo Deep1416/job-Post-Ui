7ae0fe7944807e9eabadbdbbb2ad2c489871c0a6
Deependra <deependra186037@gamil.com>
2024-08-05 10:54:26 +0530

    add render ink

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/users";
-export const JOBS_API_END_POINT = "http://localhost:8080/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+export const USER_API_END_POINT = "https://job-post-backend-jvh0.onrender.com/api/v1/users";
+export const JOBS_API_END_POINT = "https://job-post-backend-jvh0.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://job-post-backend-jvh0.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-post-backend-jvh0.onrender.com/api/v1/company";
